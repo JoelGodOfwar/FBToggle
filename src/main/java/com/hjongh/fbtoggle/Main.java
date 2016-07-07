c06bc57e976b9e5f3b0535f5164732bf52dd40db
hjongh <hrjang0916@gmail.com>
2016-07-06 22:05:48 -0700

    Upload Project

---
+++ src/main/java/com/hjongh/fbtoggle/Main.java
@@ -0,0 +1,51 @@
+package com.hjongh.fbtoggle;
+
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * A lightweight, barebones Forge mod I created to allow easy switch between default
+ * and fullbright gamma settings.
+ * 
+ * @author Jongh
+ */
+@Mod(modid = Main.MODID, name = Main.MODNAME, version = Main.VERSION)
+public class Main {
+
+    public static final String MODID = "fbtoggle";
+    public static final String MODNAME = "FBToggle";
+    public static final String VERSION = "1.0.0";
+    
+    @SidedProxy(clientSide="com.hjongh.fbtoggle.ClientProxy", serverSide="com.hjongh.fbtoggle.ServerProxy")
+    public static CommonProxy proxy;
+    
+    @Instance
+    public static Main instance = new Main();    
+     
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+                proxy.preInit(e);
+    }
+        
+    @EventHandler
+    public void init(FMLInitializationEvent e) {    	
+        proxy.init(e);
+    }
+        
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+                proxy.postInit(e);
+    }
+}
