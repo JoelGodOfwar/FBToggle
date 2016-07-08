d73b83d8b20e9a2e878297e4255eb1492a246681
hjongh <hrjang0916@gmail.com>
2016-07-08 09:49:06 -0700

    Add curse duplicate class

--- src/main/java/com/hjongh/fbtoggle/DuplicateBypass.java
+++ src/main/java/com/hjongh/fbtoggle/DuplicateBypass.java
@@ -1,5 +1,7 @@
 package com.hjongh.fbtoggle;
 
+//Useless class to bypass curse duplicate file check...
+//Pretty ridiculous if you ask me
 public class DuplicateBypass {
 	public DuplicateBypass(){
 		
