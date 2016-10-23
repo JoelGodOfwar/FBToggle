96abea02800dac2c880639d1560ebfc89b29ac3c
hjongh <hrjang0916@gmail.com>
2016-10-23 13:13:05 -0700

    remove duplicate bypass class

--- src/main/java/com/hjongh/fbtoggle/DuplicateBypass.java
+++
@@ -1,9 +0,0 @@
-package com.hjongh.fbtoggle;
-
-//Useless class to bypass curse duplicate file check...
-//Pretty ridiculous if you ask me
-public class DuplicateBypass {
-	public DuplicateBypass(){
-		
-	}
-}
